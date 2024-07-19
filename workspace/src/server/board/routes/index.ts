fe793259becdb613da7cfa122f56907c44230599
AYFG <sek3137@naver.com>
2024-07-19 16:30:09 +0900

    ejs 마무리

--- workspace/src/server/board/routes/index.ts
+++ workspace/src/server/board/routes/index.ts
@@ -1,73 +1,68 @@
 import express from 'express';
+import model from "../models/index.js"
+import createHttpError from 'http-errors';
 const router = express.Router();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
-router.get('/:type', function(req, res, next) {
-  const type = req.params.type;
-  const list = [
-    {
-      "_id": 968,
-      "title": "fdgdsf",
-      "content": "sdf",
-      "type": "info",
-      "views": 8,
-      "user": {
-        "_id": 2,
-        "name": "네오",
-        "profile": "/files/00-sample/user-neo.webp"
-      },
-      "createdAt": "2024.07.16 18:20:02",
-      "updatedAt": "2024.07.16 18:20:02",
-      "seller_id": null,
-      "repliesCount": 0,
-      "product": {
-        "image": null
-      },
-  },
-    {
-      "_id": 969,
-      "title": "test2",
-      "content": "sdf",
-      "type": "info",
-      "views": 8,
-      "user": {
-        "_id": 2,
-        "name": "몰리",
-        "profile": "/files/00-sample/user-neo.webp"
-      },
-      "createdAt": "2024.07.16 18:20:02",
-      "updatedAt": "2024.07.16 18:20:02",
-      "seller_id": null,
-      "repliesCount": 0,
-      "product": {
-        "image": null
-      },
-  },
-    {
-      "_id": 970,
-      "title": "test3",
-      "content": "sdf",
-      "type": "info",
-      "views": 8,
-      "user": {
-        "_id": 2,
-        "name": "제이지",
-        "profile": "/files/00-sample/user-neo.webp"
-      },
-      "createdAt": "2024.07.16 18:20:02",
-      "updatedAt": "2024.07.16 18:20:02",
-      "seller_id": null,
-      "repliesCount": 0,
-      "product": {
-        "image": null
-      },
-  },
-];
 
+//게시물 목록 조회
+router.get('/:type', async function(req, res, next) {
+  const type = req.params.type;
+  const list = await model.post.list(type);
   res.render('community/list', { list });
+});
+
+//게시물 상세 조회
+router.get('/:type/:_id', async function(req, res, next) {
+  const type = req.params.type;
+  const _id = req.params._id;
+  const item = await model.post.detail(Number(_id));
+  if(!item){
+    next(createHttpError(404, "게시글이 존재하지 않습니다."));
+  }
+  res.render('community/detail', { item });
+});
+//게시물 등록 화면
+router.get('/:type/new', async function(req, res, next) {
+  const type = req.params.type
+  res.render('community/new',{ type });
+});
+//게시물 등록
+router.post('/:type', async function(req, res, next) {
+  const type = req.params.type;
+  req.body.type = type;
+  await model.post.add(req.body);
+  res.redirect(`/${type}`);
+});
+//게시물 수정 화면
+router.get('/:type/:_id/edit', async function(req, res, next) {
+  const type = req.params.type;
+  const list = await model.post.list(type);
+  res.render('community/list', { list });
+});
+//게시물 수정
+router.post("/:type/:_id/edit", async function (req, res, next) {
+  const type = req.params.type;
+  const list = await model.post.list(type);;
+  res.render("community/list", { list });
+});
+//게시물 삭제
+router.post('/:type/:_id', async function(req, res, next) {
+  const type = req.params.type;
+  const list = await model.post.list(type);
+  res.render('community/list', { list });
+});
+//댓글 등록
+router.post('/:type/:_id/replies', async function(req, res, next) {
+  const {type, _id} = req.params;
+  // const _id = req.params._id;
+  const list = await model.post.addComment(Number(_id), req.body);
+  res.redirect(`/${type}/${_id}`);
 });
 
 export default router;
