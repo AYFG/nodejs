6dcd6580c283f8d375f1a8dd5c79c57bf03ffb3c
AYFG <sek82468246@gmail.com>
2024-07-22 00:52:32 +0900

    응답 헤더, 응답 전송

--- basic/1/app.js
+++ basic/1/app.js
@@ -6,7 +6,14 @@ const server = http.createServer((req,res) => {
     // localhost:3000의 요청이 없어서 node app.js를 해도 반응이 없지만 브라우저로 접속을 하면 server 함수가 실행되고 process.exit()이 실행되어 프로그램을 종료한다.
     // process.exit();
 
+    res.setHeader("Content-Type", "text/html");
+    res.write('<html>');
+    res.write('<head> <title> my First Page </title></head>');
+    res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+    res.write('</html>');
+    res.end();
 });
 
 
 server.listen(3000);
+
