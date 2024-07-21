138d53feb21fd9c4aea559c74e2da6c5ef834460
AYFG <sek82468246@gmail.com>
2024-07-22 00:46:16 +0900

    이벤트 루프, 프로세스 종료, 요청 http 메소드

--- basic/1/app.js
+++ basic/1/app.js
@@ -1,7 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req,res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+
+    // localhost:3000의 요청이 없어서 node app.js를 해도 반응이 없지만 브라우저로 접속을 하면 server 함수가 실행되고 process.exit()이 실행되어 프로그램을 종료한다.
+    // process.exit();
+
 });
 
 
