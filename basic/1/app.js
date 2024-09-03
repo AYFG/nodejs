9e5399a970a0e5ea40440354423c05e8af77c73e
AYFG <sek82468246@gmail.com>
2024-09-03 22:24:56 +0900

    라우터 요청

--- basic/1/app.js
+++ basic/1/app.js
@@ -1,19 +1,27 @@
 const http = require("http");
 
-const server = http.createServer((req,res) => {
-    console.log(req.url, req.method, req.headers);
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head> <title> my First Page </title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>",
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  // console.log(req.url, req.method, req.headers);
 
-    // localhost:3000의 요청이 없어서 node app.js를 해도 반응이 없지만 브라우저로 접속을 하면 server 함수가 실행되고 process.exit()이 실행되어 프로그램을 종료한다.
-    // process.exit();
+  // localhost:3000의 요청이 없어서 node app.js를 해도 반응이 없지만 브라우저로 접속을 하면 server 함수가 실행되고 process.exit()이 실행되어 프로그램을 종료한다.
+  // process.exit();
 
-    res.setHeader("Content-Type", "text/html");
-    res.write('<html>');
-    res.write('<head> <title> my First Page </title></head>');
-    res.write("<body><h1>Hello from my Node.js Server</h1></body>");
-    res.write('</html>');
-    res.end();
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head> <title> my First Page </title></head>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
-
 server.listen(3000);
-
