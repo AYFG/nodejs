d89033deffbc9a0282cd6177dc92303a121d2e5e
AYFG <sek82468246@gmail.com>
2024-09-04 16:22:44 +0900

    모듈 시스템 함수 하나 또는 여러 개 내보내기

--- basic/1/app.js
+++ basic/1/app.js
@@ -1,43 +1,7 @@
 const http = require("http");
-const fs = require("fs");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head> <title> my First Page </title></head>");
-    res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>",
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    }); // 이벤트를 들을 수 있다
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString(); // Buffer == 버스정류장
-      console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      // fs.writeFileSync("message.txt", message); //동기적으로 파일 생성 전까지 코드 실행을 막는 메서드
-      fs.writeFile("message.txt", message, (err) => {
-        // 비동기식
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head> <title> my First Page </title></head>");
-  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const routes = require("./routes");
+console.log(routes);
+// const server = http.createServer(routes);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
---
+++ basic/1/routes.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head> <title> my First Page </title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>Send</button></form></body>",
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    }); // 이벤트를 들을 수 있다
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString(); // Buffer == 버스정류장
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      // fs.writeFileSync("message.txt", message); //동기적으로 파일 생성 전까지 코드 실행을 막는 메서드
+      fs.writeFile("message.txt", message, (err) => {
+        // 비동기식
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head> <title> my First Page </title></head>");
+  res.write("<body><h1>Hello from my Node.js Server</h1></body>");
+  res.write("</html>");
+  res.end();
+};
+
+// module.exports = requestHandler; // 하나의 함수 내보내기
+
+// module.exports = { // 객체로 여러개 합쳐 내보내기
+//   handler: requestHandler,
+//   someText: "Some hard coded text",
+// };
+
+// module.exports.handler = requestHandler; //
+// module.exports.someText = "Some hard coded Text";
+
+exports.handler = requestHandler; // Node.js 단축키
+exports.someText = "Some hard coded Text";
