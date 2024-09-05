c686228a7eb50e339d852ad3783c08377a69d214
AYFG <sek82468246@gmail.com>
2024-09-06 00:12:02 +0900

    middleware router

--- basic/2/app.js
+++ basic/2/app.js
@@ -2,15 +2,17 @@ const express = require("express");
 
 const app = express();
 
-// 미들웨어 추가
-app.use((req, res, next) => {
-  console.log("In the middleware");
+app.use("/", (req, res, next) => {
+  console.log("This always run");
   next();
 });
-// 위의 middleware에서 next가 호출되지 않으면 넘어오지 않는다
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
   console.log("In another middleware");
-  res.send("<h1>Hello from Express</h1>");
+  res.send("<h1>The 'Add Product' Page</h1>");
+});
+app.use("/", (req, res, next) => {
+  console.log("In the middleware");
+  res.send("<h1>Hello from Express!</h1>");
 });
 
 app.listen(3000);
