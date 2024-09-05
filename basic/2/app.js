4ab707669ca05d73133bfcab4c2b20fdba4fb825
AYFG <sek82468246@gmail.com>
2024-09-06 00:07:51 +0900

    middleware

--- basic/2/app.js
+++ basic/2/app.js
@@ -1,8 +1,16 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
 
-const server = http.createServer(app);
+// 미들웨어 추가
+app.use((req, res, next) => {
+  console.log("In the middleware");
+  next();
+});
+// 위의 middleware에서 next가 호출되지 않으면 넘어오지 않는다
+app.use((req, res, next) => {
+  console.log("In another middleware");
+  res.send("<h1>Hello from Express</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
