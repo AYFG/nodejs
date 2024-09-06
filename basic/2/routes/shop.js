b81a08fdc2c143d10b8159a688aadb5a1bb05a41
AYFG <sek82468246@gmail.com>
2024-09-06 18:43:21 +0900

    html 불러오기, path.js로 경로 지정

--- basic/2/routes/shop.js
+++ basic/2/routes/shop.js
@@ -1,9 +1,12 @@
+const path = require("path");
 const express = require("express");
-
+const rootDir = require("../util/path");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
+  //   res.send("<h1>Hello from Express!</h1>");
+  //   res.sendFile("./views/shop.html");
+  res.sendFile(path.join(rootDir, "..", "2/views", "shop.html"));
 });
 
 module.exports = router;
