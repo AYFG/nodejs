678d3b49b06b68e5fb0e40eb0e2eadf677d50453
AYFG <sek82468246@gmail.com>
2024-09-06 16:59:20 +0900

    불러온 모듈 앞에 경로 /admin 추가

--- basic/2/app.js
+++ basic/2/app.js
@@ -6,7 +6,12 @@ const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+// 404 페이지
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
