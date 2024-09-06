df19440504dac95914850dd9f063d675c0cbcfce
AYFG <sek82468246@gmail.com>
2024-09-06 19:43:31 +0900

    data 공유

--- basic/2/routes/admin.js
+++ basic/2/routes/admin.js
@@ -5,6 +5,8 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   //   res.send(
@@ -16,8 +18,11 @@ router.get("/add-product", (req, res, next) => {
 
 // /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
   console.log(req.body);
   res.redirect("/");
 });
 
-module.exports = router;
+// module.exports = router;
+exports.routes = router;
+exports.products = products;
--- basic/2/routes/shop.js
+++ basic/2/routes/shop.js
@@ -1,11 +1,16 @@
 const path = require("path");
+
 const express = require("express");
+
 const rootDir = require("../util/path");
+const adminData = require("./admin");
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   //   res.send("<h1>Hello from Express!</h1>");
   //   res.sendFile("./views/shop.html");
+  console.log("shop.js", adminData.products);
   res.sendFile(path.join(rootDir, "..", "2/views", "shop.html"));
 });
 
