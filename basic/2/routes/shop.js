171cbc2f6bdda5ec7d464759c7b55c153c1cad6e
AYFG <sek82468246@gmail.com>
2024-09-08 23:26:56 +0900

    pug 동적 콘텐츠 조건부 렌더링

--- basic/2/routes/shop.js
+++ basic/2/routes/shop.js
@@ -8,11 +8,8 @@ const adminData = require("./admin");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  //   res.send("<h1>Hello from Express!</h1>");
-  //   res.sendFile("./views/shop.html");
-  // console.log("shop.js", adminData.products);
-  // res.sendFile(path.join(rootDir, "..", "2/views", "shop.html"));
-  res.render("shop");
+  const products = adminData.products;
+  res.render("shop", { prods: products, docTitle: "Shop" });
 });
 
 module.exports = router;
