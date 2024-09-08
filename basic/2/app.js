3f4e90ea4f4661ea91b34f77efc253dff051df16
AYFG <sek82468246@gmail.com>
2024-09-09 02:06:50 +0900

    레이아웃 추가

--- basic/2/app.js
+++ basic/2/app.js
@@ -19,7 +19,8 @@ app.use(shopRoutes);
 
 // 404 페이지
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "./", "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "./", "views", "404.html"));
+  res.status(404).render("404");
 });
 
 app.listen(3000);
