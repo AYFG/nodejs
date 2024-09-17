1021bcbdf0142c59afb21e4e4455b508f0619adb
AYFG <sek82468246@gmail.com>
2024-09-17 18:44:09 +0900

    MVC start

--- basic/1/routes.js
+++ basic/1/routes.js
@@ -31,6 +31,7 @@ const requestHandler = (req, res) => {
       });
     });
   }
+
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head> <title> my First Page </title></head>");
