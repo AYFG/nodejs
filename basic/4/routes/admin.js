22be50a2d5b645e35579ca6fc2259135546e8024
AYFG <sek82468246@gmail.com>
2024-09-19 01:00:06 +0900

    add product ID

---
+++ basic/4/routes/admin.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+const express = require("express");
+
+const adminController = require("../controllers/admin");
+
+const router = express.Router();
+
+router.get("/add-product", adminController.getAddProduct);
+// /admin/add-product => GET
+
+// /admin/products => GET
+router.get("/products", adminController.getProducts);
+
+// /admin/add-product => POST
+router.post("/add-product", adminController.postAddProduct);
+
+module.exports = router;
