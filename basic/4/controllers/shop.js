f8474906a27bcf16398bd2ea2d473ab679da26c2
AYFG <sek82468246@gmail.com>
2024-10-02 19:18:20 +0900

    where 조건으로 단일 제품 얻기

--- basic/4/controllers/shop.js
+++ basic/4/controllers/shop.js
@@ -17,18 +17,25 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([product]) => {
-      console.log(product);
-      res.render("shop/product-detail", {
-        product: product[0],
-        pageTitle: product.title,
-        path: "/products",
-      });
-    })
-    .catch((err) => {
-      console.error(err);
+  Product.findAll({ where: { id: prodId } }).then((products) => {
+    res.render("shop/product-detail", {
+      product: products[0],
+      pageTitle: products.title,
+      path: "/products",
     });
+  });
+  // Product.findById(prodId)
+  // .then((product) => {
+  //   console.log(product);
+  //   res.render("shop/product-detail", {
+  //     product: product,
+  //     pageTitle: product.title,
+  //     path: "/products",
+  //   });
+  // })
+  // .catch((err) => {
+  //   console.error(err);
+  // });
 };
 
 exports.getIndex = (req, res) => {
