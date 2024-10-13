4ce4248d404115fdc72c73e6c7f052046d8032ac
AYFG <sek82468246@gmail.com>
2024-10-14 00:02:59 +0900

    user 테이블 생성, 관계 설정

--- basic/4/controllers/admin.js
+++ basic/4/controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const userId = req.user.id;
+  const userId = req.user.createProduct();
   // sequelize 사용 전
   // const product = new Product(null, title, imageUrl, price, description);
   // product
