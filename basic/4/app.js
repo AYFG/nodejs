18db41889aa64fceb4ed64b7bcce8257300d644b
AYFG <sek82468246@gmail.com>
2024-10-15 14:58:48 +0900

    cart->order->orders 출력, mysql 끝

--- basic/4/app.js
+++ basic/4/app.js
@@ -11,6 +11,8 @@ const Product = require("./models/products");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 const app = express();
 
@@ -46,6 +48,9 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
 
 // db 테이블 생성 (이미 있는 테이블은 다시 생성하지 않음)
 sequelize
