9d59271f0a7e918ef2229f47bcf75913929c3ef6
AYFG <sek82468246@gmail.com>
2024-10-19 18:08:01 +0900

    모든 제품 가져오기 mongodb

--- basic/4/routes/shop.js
+++ basic/4/routes/shop.js
@@ -9,14 +9,15 @@ const router = express.Router();
 router.get("/", shopController.getIndex);
 
 router.get("/products", shopController.getProducts);
-router.get("/products/:productId", shopController.getProduct);
-router.get("/products/delete");
-router.get("/cart", shopController.getCart);
 
-router.post("/cart", shopController.postCart);
-router.post("/cart-delete-item", shopController.postCartDeleteProduct);
-router.post("/create-order", shopController.postOrder);
+// router.get("/products/:productId", shopController.getProduct);
+// router.get("/products/delete");
+// router.get("/cart", shopController.getCart);
 
-router.get("/orders", shopController.getOrders);
+// router.post("/cart", shopController.postCart);
+// router.post("/cart-delete-item", shopController.postCartDeleteProduct);
+// router.post("/create-order", shopController.postOrder);
+
+// router.get("/orders", shopController.getOrders);
 
 module.exports = router;
