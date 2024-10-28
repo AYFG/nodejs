f06a1ac8e40bb80dcf470759d9a57a49f1abe0d9
AYFG <sek82468246@gmail.com>
2024-10-28 14:07:14 +0900

    Mongoose 상품 추가

--- basic/4/app.js
+++ basic/4/app.js
@@ -21,16 +21,16 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use((req, res, next) => {
-  User.findById("671632334e82f787dccd7a4f")
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-});
+// app.use((req, res, next) => {
+//   User.findById("671632334e82f787dccd7a4f")
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -39,7 +39,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    `mongodb+srv://sek82468246:${DATABASE_PASSWORD}@cluster0.xupmv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    `mongodb+srv://sek82468246:${DATABASE_PASSWORD}@cluster0.xupmv.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0`,
   )
   .then((result) => {
     app.listen(3000);
