0a3f9a7f11c841c9b7cb95ed430b0380ea223a59
AYFG <sek82468246@gmail.com>
2024-11-02 23:46:43 +0900

    mongoose 주문 목록 가져오기, mongoose 끝

--- basic/4/controllers/shop.js
+++ basic/4/controllers/shop.js
@@ -149,8 +149,7 @@ exports.postOrder = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ "user.userId": req.user._id })
     .then((orders) => {
       console.log(orders);
       res.render("shop/orders", {
