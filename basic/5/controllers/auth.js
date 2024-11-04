5213b9820d70e123b98036ba318310b6b5a35dd7
AYFG <sek82468246@gmail.com>
2024-11-04 18:11:50 +0900

    express-session를 사용해 connect.sid 쿠키에 세션 저장

--- basic/5/controllers/auth.js
+++ basic/5/controllers/auth.js
@@ -1,6 +1,6 @@
 exports.getLogin = (req, res, next) => {
   const isLoggedIn = req.get("Cookie").split(";")[0].trim().split("=")[1];
-  console.log(req.get("Cookie"));
+  console.log(req.session.isLoggedIn);
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
@@ -9,6 +9,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true; HttpOnly");
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
