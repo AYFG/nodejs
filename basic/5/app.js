77c08fd2f077dbc1c28a32b6869203abcc32fe25
AYFG <sek82468246@gmail.com>
2024-11-06 19:35:24 +0900

    csurf 라이브러리 사용으로 CSRF 방어, * csurf는 지원이 종료됨

--- basic/5/app.js
+++ basic/5/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
 
 require("dotenv").config();
 const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
@@ -20,6 +21,7 @@ const store = new MongoDBStore({
   uri: MongoDB_URI,
   collection: "sessions",
 });
+const csrfProtection = csrf({});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -31,6 +33,8 @@ const authRoutes = require("./routes/auth");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session({ secret: "my secret", resave: false, saveUninitialized: false, store: store }));
+
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -44,6 +48,12 @@ app.use((req, res, next) => {
     .catch((err) => {
       console.error(err);
     });
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
 });
 
 app.use((req, res, next) => {
