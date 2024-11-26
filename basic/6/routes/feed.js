b2b5bfaf0baf3ce9931eb1acc332cc864ab2eed9
AYFG <sek82468246@gmail.com>
2024-11-26 21:47:11 +0900

    get,post restapi bruno 테스트

--- basic/6/routes/feed.js
+++ basic/6/routes/feed.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
-const feedController = require("./controllers/feed");
+const feedController = require("../controllers/feed");
 
-const router = express.router();
+const router = express.Router();
+
 // GET /feed/posts
 router.get("/posts", feedController.getPosts);
+
+// POST /feed/posts
+router.post("/posts", feedController.createPost);
 
 module.exports = router;
