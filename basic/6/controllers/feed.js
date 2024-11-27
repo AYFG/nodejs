9f1b3f3cbcfef99a6df2db46b9f1737119f44f3e
AYFG <sek82468246@gmail.com>
2024-11-28 02:14:30 +0900

    서버 측 유효성 검증 추가

--- basic/6/controllers/feed.js
+++ basic/6/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,6 +18,15 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(422)
+      .json({
+        message: "유효성 검사 실패, 입력한 데이터가 올바르지 않습니다",
+        errors: errors.array(),
+      });
+  }
   const title = req.body.title;
   const content = req.body.content;
   // Create post in db
