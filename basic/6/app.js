0fba001f88738229296008d8e11a6244a01e4d4d
AYFG <sek82468246@gmail.com>
2024-11-27 16:59:05 +0900

    html 파일 생성, live server로 테스트, CORS 오류를 미들웨어에 응답 헤더 추가하여 해결

--- basic/6/app.js
+++ basic/6/app.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use("/feed", feedRoutes);
 
 app.listen(8080);
