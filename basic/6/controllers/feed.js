ed2233484ac0f167d3bb2314e1a6c918360ac431
AYFG <sek82468246@gmail.com>
2024-11-28 16:47:44 +0900

    더미 렌더링 지우고 db 데이터 렌더링, post, 에러 핸들링

--- basic/6/controllers/feed.js
+++ basic/6/controllers/feed.js
@@ -1,45 +1,73 @@
 const { validationResult } = require("express-validator");
 
+const Post = require("../models/post");
+const post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: "1",
-        title: "First Post",
-        content: "This is the first post",
-        imageUrl: "images/starfish.png",
-        creator: {
-          name: "Woong",
-        },
-        createdAt: new Date(),
-      },
-    ],
-  });
+  Post.find()
+    .then((posts) => {
+      res.status(200).json({
+        message: "전체 게시물을 성공적으로 가져왔습니다.",
+        posts: posts,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res
-      .status(422)
-      .json({
-        message: "유효성 검사 실패, 입력한 데이터가 올바르지 않습니다",
-        errors: errors.array(),
-      });
+    const error = new Error("유효성 검사 실패, 입력한 데이터가 올바르지 않습니다");
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully",
-    post: {
-      _id: new Date().toISOString().replace(/:/g, "-"),
-      title: title,
-      content: content,
-      creator: {
-        name: "Woong",
-      },
-      createdAt: new Date(),
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/starfish.png",
+    creator: {
+      name: "Woong",
     },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const error = new Error("존재하지 않는 게시물 입니다.");
+        error.statusCode = 404;
+        throw error;
+      }
+      res.status(200).json({ message: "게시물을 성공적으로 가져왔습니다.", post: post });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
