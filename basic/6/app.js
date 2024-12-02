51997299fd25abdd9a94c22aaac4e3cac08e6d36
AYFG <sek82468246@gmail.com>
2024-12-02 19:23:09 +0900

    회원가입

--- basic/6/app.js
+++ basic/6/app.js
@@ -12,6 +12,7 @@ const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
 const MongoDB_URI = `mongodb+srv://sek82468246:${DATABASE_PASSWORD}@cluster0.xupmv.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0`;
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -48,12 +49,14 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
---
+++ basic/6/controllers/auth.js
@@ -0,0 +1,37 @@
+const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/user");
+
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("검증 실패");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        name: name,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({ message: "유저 생성 성공", userId: result._id });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
