74dfa9a0d9191593868225811990356d7946f7df
AYFG <sek82468246@gmail.com>
2024-12-09 16:32:13 +0900

    front graphql 연결, 연결에 성공했어도  이미 있는 이메일이거나 등등 입력 검증에 실패하면 상태 코드는 200이고 body 안에 errros 배열로 검증 실패 내용이나 error를 알려준다.

--- basic/7/graphql/resolvers.js
+++ basic/7/graphql/resolvers.js
@@ -10,19 +10,20 @@ module.exports = {
       errors.push({ message: "이메일이 잘못되었습니다" });
     }
     if (
-      !validator.isEmpty(userInput.password) ||
+      validator.isEmpty(userInput.password) ||
       !validator.isLength(userInput.password, { min: 5 })
     ) {
       errors.push({ message: "비밀번호가 너무 짧습니다." });
-    }
-    const existingUser = await User.findOne({ email: userInput.email });
-    if (existingUser) {
-      const error = new Error("이미 사용중인 이메일입니다.");
     }
     if (errors.length > 0) {
       const error = new Error("잘못된 입력이 있습니다.");
       error.data = errors;
       error.code = 422;
+      throw error;
+    }
+    const existingUser = await User.findOne({ email: userInput.email });
+    if (existingUser) {
+      const error = new Error("이미 사용중인 이메일입니다.");
       throw error;
     }
     const hashedPw = await bcrypt.hash(userInput.password, 12);
