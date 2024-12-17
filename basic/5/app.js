bc07822dd034c1d57fea841b3506ef95a4905f33
AYFG <sek82468246@gmail.com>
2024-12-17 23:18:14 +0900

    test 실행 프레임워크 mocha, 테스트 조건 정의 chai

--- basic/5/app.js
+++ basic/5/app.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const fs = require("fs");
+const https = require("https");
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -32,6 +33,9 @@ const store = new MongoDBStore({
   collection: "sessions",
 });
 const csrfProtection = csrf({});
+
+const privateKey = fs.readFileSync("server.key");
+const certificate = fs.readFileSync("server.cert");
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -119,6 +123,7 @@ mongoose
   .connect(MongoDB_URI)
   .then((result) => {
     app.listen(PORT || 3000);
+    // https.createServer({ key: privateKey, cert: certificate }, app).listen(PORT || 3000);
   })
   .catch((err) => {
     console.error(err);
