66460dd1b92c1aea691925ae50f5a90ee9f9ba65
AYFG <sek82468246@gmail.com>
2024-12-23 16:58:16 +0900

    모카 라이프사이클 훅

--- basic/6/test/auth-controller.js
+++ basic/6/test/auth-controller.js
@@ -6,6 +6,28 @@ const User = require("../models/user");
 const AuthController = require("../controllers/auth");
 
 describe("Auth Controller - Login", function () {
+  before(function (done) {
+    require("dotenv").config();
+    const DATABASE_ID = process.env.DATABASE_ID;
+    const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+    const MongoDB_URI = `mongodb+srv://${DATABASE_ID}:${DATABASE_PASSWORD}@cluster0.xupmv.mongodb.net/test-messages?retryWrites=true&w=majority&appName=Cluster0`;
+    mongoose
+      .connect(MongoDB_URI)
+      .then((result) => {
+        const user = new User({
+          email: "test@test.com",
+          password: "tester",
+          name: "Test",
+          posts: [],
+          _id: "6756c46008f7e34a029ae7a3",
+        });
+        return user.save();
+      })
+      .then(() => {
+        done();
+      });
+  });
+
   it("데이터베이스에 접근하지 못하면 코드 500에 오류를 제기해야 합니다", function (done) {
     sinon.stub(User, "findOne");
     User.findOne.rejects();
@@ -34,42 +56,33 @@ describe("Auth Controller - Login", function () {
     const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
     const MongoDB_URI = `mongodb+srv://${DATABASE_ID}:${DATABASE_PASSWORD}@cluster0.xupmv.mongodb.net/test-messages?retryWrites=true&w=majority&appName=Cluster0`;
 
-    mongoose
-      .connect(MongoDB_URI)
-      .then((result) => {
-        const user = new User({
-          email: "test@test.com",
-          password: "tester",
-          name: "Test",
-          posts: [],
-          _id: "6756c46008f7e34a029ae7a3",
-        });
-        return user.save();
+    const req = { userId: "6756c46008f7e34a029ae7a3" };
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status: function (code) {
+        this.statusCode = code;
+        return this;
+      },
+      json: function (data) {
+        this.userStatus = data.status;
+      },
+    };
+    AuthController.getUserStatus(req, res, () => {}).then(() => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.userStatus).to.be.equal("I am new!");
+      done();
+    });
+  });
+  after(function (done) {
+    User.deleteMany({})
+      .then(() => {
+        return mongoose.disconnect();
       })
       .then(() => {
-        const req = { userId: "6756c46008f7e34a029ae7a3" };
-        const res = {
-          statusCode: 500,
-          userStatus: null,
-          status: function (code) {
-            this.statusCode = code;
-            return this;
-          },
-          json: function (data) {
-            this.userStatus = data.status;
-          },
-        };
-        AuthController.getUserStatus(req, res, () => {}).then(() => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.userStatus).to.be.equal("I am new!");
-          User.deleteMany({}).then(() => {
-            return mongoose.disconnect().then(() => {
-              done();
-            });
-          });
-        });
-      })
-
-      .catch((err) => console.error(err));
+        done();
+      });
   });
+  beforeEach(function () {});
+  afterEach(function () {});
 });
